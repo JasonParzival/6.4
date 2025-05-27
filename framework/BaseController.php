5105275a26df7ef116b42161f427d054ff4c2760
JasonParzival <ruslanmuhamed123@gmail.com>
2025-05-27 18:55:51 +0800

    Сделал перенаправление и исправил немного историю

--- framework/BaseController.php
+++ framework/BaseController.php
@@ -21,6 +21,23 @@ abstract class BaseController {
         session_set_cookie_params(60*60*10);
         session_start();
 
+        $currentUrl = $_SERVER['REQUEST_URI'];
+        if (!isset($_SESSION['history'])) {
+            $_SESSION['history'] = [];
+        }
+
+        if (isset($_SESSION['is_logged']) && $_SESSION['is_logged'] && $currentUrl === '/login') {
+            header("Location: /");
+            exit;
+        }
+
+        if ($currentUrl !== '/login' && !isset($_SESSION['is_logged'])) {
+            header("Location: /login");
+            exit;
+        }
+
+        $context["history"] = isset($_SESSION['history']) ? $_SESSION['history'] : "";
+
         $method = $_SERVER['REQUEST_METHOD'];
         $context = $this->getContext(); // вызываю context тут
         if ($method == 'GET') {
